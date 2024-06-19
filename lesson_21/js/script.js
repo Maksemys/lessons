4d2a9f87d199daa6cb759acf66c74b54ab4ff748
Maksemys <158852742+Maksemys@users.noreply.github.com>
2024-06-19 18:28:50 +0300

    Add files via upload

---
+++ lesson_21/js/script.js
@@ -0,0 +1,16 @@
+// Сторогий режим
+"use strict"
+
+// let user-name
+// let UserName
+// let userName = 20
+
+// const userName = 20
+// userName = "Іштван"
+
+let numOne = 20
+let numTwo = "20"
+
+let numSumm = numOne + numTwo
+console.log(numSumm)
+console.log(typeof numSumm)
